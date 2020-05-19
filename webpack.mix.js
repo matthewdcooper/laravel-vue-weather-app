00315ae20d1a8de6ac65d72b7db59adfcc9b8bf7
matthew <matthew@dcooper.co.uk>
2020-05-19 09:54:45 +0100

    hello browser sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .browserSync({
-        proxy: '127.0.0.1'
+        proxy: 'localhost:8000'
     });
